9fc90eb40edba61eed5d4de7d28efc6b123b40ff
Sara-Morales <moralessaramelissa@gmail.com>
2024-06-12 22:40:36 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:CarlosMatiz2/Taller_Final_Ces4.git main:gh-pages
+git push -f git@github.com:SaraMorales25/Taller_Final_Ces4.git main:gh-pages
 
 cd -
